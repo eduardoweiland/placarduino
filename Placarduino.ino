510622bddcfdd4d61b4ac6ba384a8c7cc0732d12
Eduardo Weiland <eduardo@eduardoweiland.info>
2017-10-27 21:37:26 -0200

    Adicionar som com um buzzer 5V

--- Placarduino.ino
+++ Placarduino.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <Tone.h>
 
 /*
  * CONSTANTES
@@ -15,6 +16,7 @@
 #define PIN_BTN_1         7
 #define PIN_BTN_2         6
 
+#define PIN_BUZZER        5
 
 /*
  * GLOBAIS
@@ -39,6 +41,9 @@ const char secondPlayerName[] = "Jogador 2";
 unsigned int secondPlayerScore = 0;
 int secondButtonState = LOW;
 
+// Objeto para controle de um buzzer
+Tone buzzer;
+
 
 /*
  * SETUP
@@ -47,7 +52,10 @@ int secondButtonState = LOW;
 void setup() {
     setupInputs();
     setupLCD();
+    setupBuzzer();
+
     printWelcome();
+    playWelcome();
 
     delay(3000);
 }
@@ -81,6 +89,11 @@ void setupLCD()
     lcd.begin(16, 2);
 }
 
+void setupBuzzer()
+{
+    buzzer.begin(PIN_BUZZER);
+}
+
 void printWelcome()
 {
     lcd.clear();
@@ -94,6 +107,72 @@ void printWelcome()
     lcd.print("a.k.a. DAMN");
 }
 
+// Escala de Dó maior
+void playWelcome()
+{
+    // Básico
+    int i;
+    int notes[] = { NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6, NOTE_C7 };
+
+    for (i = 0; i < 8; ++i) {
+        buzzer.play(notes[i], 30 + (i * 5));
+        delay(35 + (i * 5));
+    }
+}
+
+// Super Mário
+//void playWelcome()
+//{
+//    buzzer.play(NOTE_E6, 120);
+//    delay(140);
+//    buzzer.play(NOTE_E6, 120);
+//    delay(140);
+//    delay(140);
+//    buzzer.play(NOTE_E6, 120);
+//    delay(140);
+//    delay(140);
+//    buzzer.play(NOTE_C6, 120);
+//    delay(140);
+//    buzzer.play(NOTE_E6, 240);
+//    delay(260);
+//    buzzer.play(NOTE_G6, 240);
+//    delay(260);
+//    delay(260);
+//    buzzer.play(NOTE_G5, 240);
+//    delay(260);
+//}
+
+// Sweet Child O' Mine
+//void playWelcome()
+//{
+//    int i, j;
+//    int firstNote[] = { NOTE_CS5, NOTE_DS5, NOTE_FS5 };
+//
+//    for (i = 0; i < 3; ++i) {
+//        for (j = 0; j < 2; ++j) {
+//            buzzer.play(firstNote[i], 240);
+//            delay(260);
+//            buzzer.play(NOTE_CS6, 240);
+//            delay(260);
+//            buzzer.play(NOTE_GS5, 240);
+//            delay(260);
+//            buzzer.play(NOTE_FS5, 240);
+//            delay(260);
+//            buzzer.play(NOTE_FS6, 240);
+//            delay(260);
+//            buzzer.play(NOTE_GS5, 240);
+//            delay(260);
+//            buzzer.play(NOTE_F6, 240);
+//            delay(260);
+//            buzzer.play(NOTE_GS5, 240);
+//            delay(260);
+//        }
+//    }
+//
+//    buzzer.play(NOTE_CS5, 240);
+//    delay(260);
+//}
+
 void checkButtons()
 {
     int newButtonState;
@@ -102,6 +181,7 @@ void checkButtons()
     if (newButtonState != firstButtonState && newButtonState == HIGH) {
         // Se o botão mudou de estado e o novo estado é pressionado, soma um ponto
         firstPlayerScore++;
+        playFeedback();
     }
     firstButtonState = newButtonState;
 
@@ -109,6 +189,7 @@ void checkButtons()
     if (newButtonState != secondButtonState && newButtonState == HIGH) {
         // Se o botão mudou de estado e o novo estado é pressionado, soma um ponto
         secondPlayerScore++;
+        playFeedback();
     }
     secondButtonState = newButtonState;
 }
@@ -130,4 +211,13 @@ void printScore()
     lcd.print(secondPlayerName);
     lcd.setCursor(13, 1);
     lcd.print(dtostrf(secondPlayerScore, 3, 0, outScore));
+}
+
+void playFeedback()
+{
+    buzzer.play(NOTE_C6, 120);
+    delay(140);
+
+    buzzer.play(NOTE_C7, 420);
+    delay(500);
 }
