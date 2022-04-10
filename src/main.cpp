062bdc6adacf5f137b392b057eabfe96bc6c8875
Eduardo Weiland <eduardo@eduardoweiland.info>
2022-04-10 11:46:10 -0300

    Possibilitar execução do PlayerCardCreator durante boot
    Mantendo o botão frontal pressionado enquanto o Placarduino
    é ligado, o sistema irá bootar no modo de criação de novos
    cartões de usuários. Para voltar ao jogo, é necessário
    desligar e ligar novamente o Placarduino.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
+#include "config.h"
 #include "App.hpp"
 #include "Placarduino.hpp"
+#include "PlayerCardCreator.hpp"
 
 Placarduino placarduino;
+PlayerCardCreator cardCreator;
+App *app = &placarduino;
 
 void setup()
 {
-    placarduino.init();
+    if (digitalRead(PIN_BTN_FRONT) == HIGH) {
+        app = &cardCreator;
+    }
+
+    app->init();
 }
 
 void loop()
 {
-    placarduino.run();
+    app->run();
 }
